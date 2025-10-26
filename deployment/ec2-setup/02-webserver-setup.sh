b35b8bc71f18f62ad055225d7f8cdc0c8f0b559c
jc-pena <jc.pena@uniandes.edu.co>
2025-10-26 13:58:25 -0500

    Add poetry lock regeneration step to deployment scripts

--- deployment/ec2-setup/02-webserver-setup.sh
+++ deployment/ec2-setup/02-webserver-setup.sh
@@ -187,6 +187,9 @@ sudo -u appuser bash -c "cd /home/appuser/MISO4204-Desarrollo_Nube && .venv/bin/
 print_info "Configurando poetry..."
 sudo -u appuser bash -c "cd /home/appuser/MISO4204-Desarrollo_Nube && .venv/bin/poetry config virtualenvs.create false"
 
+print_info "Regenerando poetry.lock..."
+sudo -u appuser bash -c "cd /home/appuser/MISO4204-Desarrollo_Nube && .venv/bin/poetry lock --no-update"
+
 print_info "Instalando dependencias (esto puede tomar varios minutos)..."
 sudo -u appuser bash -c "cd /home/appuser/MISO4204-Desarrollo_Nube && .venv/bin/poetry install --only main"
 
